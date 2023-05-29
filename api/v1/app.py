e27236756de8cebefa212318beff55e96fc822cf
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-29 14:05:19 -0700

    flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-app = Flask('__name__')
+app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources=r"/api/v1/*", origins="*")
@@ -20,11 +20,11 @@ def not_found(error):
 
 @app.teardown_appcontext
 def close_storage(*args, **kwargs):
-    """Clost app storage (FileStorage or DBStorage)"""
+    """Close app storage (FileStorage or DBStorage)"""
     storage.close()
 
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
+    port = getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=int(port), threaded=True)
