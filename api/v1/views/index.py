7c86132a06d0fe4311d59912327db5c4f17914e7
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-30 10:39:26 -0700

    text

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,13 +3,13 @@
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
-from sqlalchemy.sql import text
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
@@ -30,5 +30,5 @@ def get_stats():
     }
     return_dict = {}
     for key, value in stats.items():
-        return_dict[key] = storage.count(text(value))
+        return_dict[key] = storage.count(value)
     return jsonify(return_dict)
