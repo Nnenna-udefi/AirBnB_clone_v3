6652bc430bcd2695561b3abeb9d3008f9c3a457e
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-30 00:07:18 -0700

    status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """app_views index file"""
 from api.v1.views import app_views
 from flask import jsonify
+from models impoet storage
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """method to get status and return a JSON file"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def get_count():
+    """retrieves the number of each objects by type"""
+    stats = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    return jsonify({name: storage.count(cls) for cls, name in stats.items()})
