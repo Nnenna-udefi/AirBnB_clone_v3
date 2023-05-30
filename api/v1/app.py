0d70d05c4408ec5c2558ff4c9af760943d7b3a24
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-29 23:44:53 -0700

    url

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
