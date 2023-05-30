eb6197797872e8e0bb33443faedf918b51af4ca2
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-30 05:56:53 -0700

    db

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,19 +3,18 @@
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
-from sqlalchemy import text
 
 
 @app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
+def get_status():
     """hbnbStatus"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    hbnbText = {
+def get_stats():
+    """ get stats"""
+    stats = {
         "amenities": "Amenity",
         "cities": "City",
         "places": "Place",
@@ -24,10 +23,6 @@ def hbnbStats():
         "users": "User"
     }
     return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(text(value))
+    for key, value in stats.items():
+        return_dict[key] = storage.count(value)
     return jsonify(return_dict)
-
-
-if __name__ == "__main__":
-    pass
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -26,18 +26,17 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """Instantiate a DBStorage object"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(HBNB_MYSQL_USER,
-                                             HBNB_MYSQL_PWD,
-                                             HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
-        if HBNB_ENV == "test":
+        """creates a public instance"""
+        user = getenv('HBNB_MYSQL_USER')
+        passwd = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+        env = getenv('HBNB_ENV')
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+        if env == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
